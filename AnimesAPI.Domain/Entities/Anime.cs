5d58bca6e29209ea864781516c88d99a0ebea379
jonhferreira <jonhifs@gmail.com>
2025-02-18 20:01:06 -0300

    feat: domain base definition

---
+++ AnimesAPI.Domain/Entities/Anime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimesAPI.Domain.Entities
+{
+    public sealed class Anime
+    {
+        public int Id { get;  set; }
+        public string Name { get;  set; }
+        public string Director { get;  set; }
+        public string Description { get;  set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ AnimesAPI.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimesAPI.Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> Create(T obj);
+        Task<T> Update(T obj);
+        Task Delete(T obj);
+        Task<List<T>> GetAll();
+
+    }
+}
