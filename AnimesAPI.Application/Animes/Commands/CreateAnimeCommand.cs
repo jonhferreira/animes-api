b941bdb389f7f071a2e9a661818e6f79f97db7bb
jonhferreira <jonhifs@gmail.com>
2025-02-19 00:55:09 -0300

    feat: CQRS base creation

---
+++ AnimesAPI.Application/Animes/Commands/CreateAnimeCommand.cs
@@ -0,0 +1,17 @@
+﻿using AnimesAPI.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimesAPI.Application.Animes.Commands
+{
+    public class CreateAnimeCommand : IRequest<Anime>
+    {
+        public string Name {  get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+    }
+}
---
+++ AnimesAPI.Application/Animes/Commands/CreateAnimeCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AnimesAPI.Domain.Entities;
+using AnimesAPI.Domain.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimesAPI.Application.Animes.Commands
+{
+    public class CreateAnimeCommandHandler : IRequestHandler<CreateAnimeCommand, Anime>
+    {
+        private readonly IAnimeRepository _animeRepository;
+        public CreateAnimeCommandHandler(IAnimeRepository animeRepository) 
+        {
+            _animeRepository = animeRepository;
+        }
+        public Task<Anime> Handle(CreateAnimeCommand request, CancellationToken cancellationToken)
+        {
+            var anime = new Anime()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Director= request.Director
+            };
+
+            return _animeRepository.Create(anime);
+
+        }
+    }
+}
