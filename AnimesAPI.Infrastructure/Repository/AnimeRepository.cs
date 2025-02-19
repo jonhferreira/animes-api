72cabc6bffb894b6c596e2be0872fdc170277f67
jonhferreira <jonhifs@gmail.com>
2025-02-18 21:15:41 -0300

    feat: anime repository definition

---
+++ AnimesAPI.Infrastructure/Repository/AnimeRepository.cs
@@ -0,0 +1,38 @@
+﻿using AnimesAPI.Domain.Entities;
+using AnimesAPI.Domain.Interfaces.Repositories;
+using AnimesAPI.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimesAPI.Infrastructure.Repository
+{
+    public class AnimeRepository : BaseRepository<Anime>, IAnimeRepository
+    {
+        private readonly AnimesDBContext _animeDbContext;
+        public AnimeRepository(AnimesDBContext context) : base(context)
+        {
+            _animeDbContext = context;
+        }
+
+        public async Task<List<Anime>> Get(int id, string director, string name)
+        {
+            var animes = await _animeDbContext.Animes.Where(a => a.Id == id || a.Director == director || a.Name == name).ToListAsync();
+
+            return animes;
+        }
+
+        public async Task SoftDelete(int id)
+        {
+            var anime = await _animeDbContext.Animes.FindAsync(id);
+            
+            anime.IsDeleted = true;
+
+            _animeDbContext.SaveChanges
+
+        }
+    }
+}
---
+++ AnimesAPI.Infrastructure/Repository/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using AnimesAPI.Domain.Interfaces.Repositories;
+using AnimesAPI.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace AnimesAPI.Infrastructure.Repository;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    private readonly AnimesDBContext _context;
+
+    public BaseRepository(AnimesDBContext context)
+    {
+        _context = context;
+    }
+    public async Task<T> Create(T obj)
+    {
+        _context.Add(obj);
+        await _context.SaveChangesAsync();
+        return obj;
+    }
+
+    public async Task Delete(T obj)
+    {
+        _context.Remove(obj);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<T>> GetAll()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task<T> Update(T obj)
+    {
+        _context.Entry(obj).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return obj;
+    }
+}
