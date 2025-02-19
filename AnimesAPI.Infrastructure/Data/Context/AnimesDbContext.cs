ea06cbcd8230955d63ffaae336a1c4e072b30aa4
jonhferreira <jonhifs@gmail.com>
2025-02-18 21:15:05 -0300

    feat: entity framework context configuration

---
+++ AnimesAPI.Infrastructure/Data/Context/AnimesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AnimesAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace AnimesAPI.Infrastructure.Data.Context;
+
+public class AnimesDBContext : DbContext
+{
+    public AnimesDBContext(DbContextOptions<AnimesDBContext> options) : base(options) {}
+
+    public DbSet<Anime> Animes { get; set; }
+}
